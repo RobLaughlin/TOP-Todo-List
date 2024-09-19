6a0f2e63d22c0e4e30a6de4cdc5b78b54e70819c
Rob Laughlin <waterfallkicks@gmail.com>
2024-09-19 07:25:00 -0700

    Initialize basic Todo class structure

---
+++ src/js/Todos.js
@@ -0,0 +1,87 @@
+import PubSub from "./PubSub";
+
+export class Todo {
+    #title;
+    #description;
+    #dueDate;
+    #priority;
+    #notes;
+    #onChange;
+
+    /**
+     * Creates a Todo
+     * 
+     * @param {string} title The title of the Todo 
+     * @param {string} description The description of the Todo
+     * @param {Date} dueDate The due date of the Todo
+     * @param {number} priority The priority of the Todo (an integer, lower is higher priority)
+     * @param {string} [notes=""] Optional notes to include with the Todo
+     * @param {?string} [onChange=null] The event to publish when a todo is changed.
+     * If onChange is null, then no event will be published. 
+     * Otherwise, publish using the current Todo object as data.
+     */
+    constructor(title, description, dueDate, priority, notes="", onChange=null) {
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+        this.#notes = notes;
+        this.#onChange = onChange;
+    }
+
+    get title() { return this.#title; }
+    set title(value) {
+        this.#title = value;
+        if (this.#onChange !== null) { PubSub.publish(this.#onChange, this) };
+    }
+
+    get description() { return this.#description; }
+    set description(value) {
+        this.#description = value;
+        if (this.#onChange !== null) { PubSub.publish(this.#onChange, this) };
+    }
+
+    get dueDate() { return this.#dueDate; }
+    set dueDate(value) {
+        this.#dueDate = value;
+        if (this.#onChange !== null) { PubSub.publish(this.#onChange, this) };
+    }
+
+    get priority() { return this.#priority; }
+    set priority(value) {
+        this.#priority = value;
+        if (this.#onChange !== null) { PubSub.publish(this.#onChange, this) };
+    }
+
+    get notes() { return this.#notes; }
+    set notes(value) {
+        this.#notes = value;
+        if (this.#onChange !== null) { PubSub.publish(this.#onChange, this) };
+    }
+}
+
+export class Project {
+    #todos
+
+    /**
+     * 
+     * @param {Todo[]} [todos=[]] An array of Todos 
+     */
+    constructor(todos=[]) {
+        this.#todos = todos.map(todo => ({...todo}));    
+    }
+}
+
+export class ProjectList {
+    #projects
+
+    /**
+     * 
+     * @param {Project[]} projects An array of projects 
+     */
+    constructor(projects=[]) {
+        this.#projects = projects.map(project => ({...project}));
+    }
+}
+
+new Todo()
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import { Todo } from "./Todos";
+
+const todo = new Todo("Title", "Desc", new Date(), 0, "...notes");
+console.log(todo);
