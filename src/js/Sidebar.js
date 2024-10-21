f7ad55f4b9093527e2069f6001fa2f7b4b0249f0
Rob Laughlin <waterfallkicks@gmail.com>
2024-10-21 07:33:39 -0700

    Rework sidebar into a proper component

--- src/js/Sidebar.js
+++ src/js/Sidebar.js
@@ -1,6 +1,7 @@
 import folder from "../icons/folder.svg";
 import minusBox from "../icons/minus-box.svg";
 import note from "../icons/note.svg";
+import magnify from "../icons/magnify.svg";
 import sanitizeHtml from "sanitize-html";
 import PubSub from "./PubSub";
 import { Project, Todo } from "./Todos";
@@ -8,15 +9,6 @@ import { Project, Todo } from "./Todos";
 export const PROJECTS_CHANGED_EVENT = "projectsChanged";
 export const PROJECT_CHANGED_EVENT = "projectChanged";
 export const TODO_CHANGED_EVENT = "todoChanged";
-
-/**
- * Event that gets triggered when the projects array gets changed
- * @param {Project[]} projects The array of projects that was changed
- */
-function projectsChanged(projects) {
-    renderSidebar(projects);
-}
-PubSub.subscribe(PROJECTS_CHANGED_EVENT, projectsChanged);
 
 /**
  * Event that gets triggered when a project gets changed
@@ -45,7 +37,7 @@ function projectRemoved(e, projects) {
     let clickedProject = e.target.parentElement;
     let clickedItemContainer = clickedProject.parentElement;
     const UUID = clickedProject.dataset.uuid;
-    console.log(projects);
+ 
     for (let i = 0; i < projects.length; i++) {
         const project = projects[i];
         if (project.uuid === UUID) {
@@ -64,7 +56,7 @@ function projectRemoved(e, projects) {
 function todoRemoved(e, project) {
     let clickedTodo = e.target.parentElement;
     const UUID = clickedTodo.dataset.uuid;
-    console.log(project);
+
     for (let i = 0; i < project.size(); i++) {
         const todo = project.get(i);
         if (todo.uuid === UUID) {
@@ -75,6 +67,10 @@ function todoRemoved(e, project) {
     }
 }
 
+/**
+ * The event called when a folder icon is clicked on a project
+ * @param {Event} e The folder icon click event for projects
+ */
 function folderBtnClicked(e) {
     let projectContainer = e.target.parentElement.parentElement;
     let todos = projectContainer.querySelectorAll(".todo");
@@ -84,7 +80,49 @@ function folderBtnClicked(e) {
 }
 
 /**
- * 
+ * The event called when the search bar on the sidebar is focused
+ * @param {Event} e Focused event for the search bar
+ */
+function searchBarFocused(e) {
+    e.preventDefault();
+
+    let searchBar = e.target.parentElement;
+    searchBar.classList.add("focused");
+}
+
+/**
+ * The event called when the search bar on the sidebar is unfocused
+ * @param {Event} e Unfocused event for the search bar 
+ */
+function searchBarUnFocused(e) {
+    e.preventDefault();
+
+    let searchBar = e.target.parentElement;
+    searchBar.classList.remove("focused");
+}
+
+/**
+ * The sidebar template
+ * @returns Template to generate the sidebar component HTML
+ */
+const sidebarTemplate = () => {
+    return `
+        <div class="sidebar">
+            <div class="titleContainer">
+                <h1 class="title">Projects</h1>
+            </div>
+            <div class="searchContainer">
+                <div class="searchBar">
+                    <img src="${magnify}" alt="Magnifying glass icon" class="icon">
+                    <input type="search" placeholder="Search" class="search">
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+/**
+ * The project template
  * @param {Project} project
  * @returns Sanitized Project HTML to inject into sidebar
  */
@@ -99,7 +137,7 @@ export const projectSidebarTemplate = project => {
 }
 
 /**
- * 
+ * The todo template
  * @param {Todo} todo
  * @returns Sanitized Todo HTML to inject into sidebar
  */
@@ -114,64 +152,11 @@ export const todoSidebarTemplate = todo => {
 }
 
 /**
- * Generate project HTML from an array of projects and injects them in order into the sidebar component.
- * Injects the HTML into the div elements with the .sidebar class.
- * @param {Project[]} projects The array of projects to be rendered to the sidebar 
+ * Generates a number of projects and todos to use as test data
+ * @param {number} numProjects The number of projects to generate 
+ * @param {number} numTodos The number of todos to generate within each project
+ * @returns {Project[]} The array of test projects generated
  */
-export const renderSidebar = (projects) => {
-    let sidebars = document.querySelectorAll(".sidebar");
-
-    // Clear old sidebar data
-    let sidebarItems = document.querySelectorAll(".sidebar .itemContainer");
-    sidebarItems.forEach(item => {
-        item.remove();
-    });
-
-    // Inject projects and todos
-    for (let p = 0; p < projects.length; p++) {
-        let project = projects[p];
-        let itemContainer = document.createElement("div");
-        itemContainer.classList.add("itemContainer");
-
-        // Generate project html
-        let projectTemplate = document.createElement("template");
-        projectTemplate.innerHTML = projectSidebarTemplate(project, p);
-        let projectNode = projectTemplate.content.querySelector("div");
-       
-        // Add folder button click event handler
-        let folderBtn = projectNode.querySelector(".folder.icon");
-        folderBtn.addEventListener("click", folderBtnClicked);
-
-        // Add remove button click event handler
-        let removeProjectBtn = projectNode.querySelector(".removeProjectIcon");
-        removeProjectBtn.addEventListener("click", e => {
-            projectRemoved(e, projects);
-        });
-        itemContainer.appendChild(projectNode);
-
-        // Generate todo html
-        for (let t = 0; t < project.todos.length; t++) {
-            let todoHtml = todoSidebarTemplate(project.todos[t], t);
-            let todoTemplate = document.createElement("template");
-            todoTemplate.innerHTML = todoHtml;
-            let todoNode = todoTemplate.content.querySelector("div");
-            let removeTodoBtn = todoNode.querySelector(".removeProjectIcon");
-            removeTodoBtn.addEventListener("click", e => {
-                todoRemoved(e, project);
-            });
-            itemContainer.appendChild(todoNode);
-        } 
-
-        // Inject html
-        sidebars.forEach(sidebar => {
-            sidebar.appendChild(itemContainer);
-        });
-
-        // Open the default/first project
-        if (p === 0) { folderBtn.click(); }
-    }
-}
-
 export const createTestProjects = (numProjects, numTodos) => {
     let projects = []
     for (let i = 0; i < numProjects; i++) {
@@ -181,5 +166,106 @@ export const createTestProjects = (numProjects, numTodos) => {
         }
         projects.push(project);
     }
-    return projects;
+
+    const selected = numProjects >= 1 ? projects[0].uuid : null;
+    return { projects, selected };
+};
+
+/**
+ * The main sidebar component
+ * @param {Project[]} projects The array of projects to be loaded into the sidebar 
+ * @returns {Object} The sidebar component
+ */
+export const sidebar = (projects) => {
+    let selected = projects.length > 0 ? projects[0].uuid : null;
+
+    return (() => {
+        let sidebarObj = {
+            projects,
+
+            /**
+             * Selects a project from the sidebar
+             * @param {string} uuid The UUID of the project to be selected
+             */
+            select: (uuid) => {
+                projects.forEach(project => {
+                    if (project.uuid === uuid) {
+                        selected = uuid;
+                        return project;
+                    }
+                });
+                throw new Error(`Project UUID ${uuid} not found.`);
+            },
+
+            /**
+             * 
+             * @returns 
+             */
+            html: () => {
+                let sidebarTemplateNode = document.createElement("template");
+                sidebarTemplateNode.innerHTML = sidebarTemplate();
+                let sidebarNode = sidebarTemplateNode.content.querySelector("div");
+                
+                // Implement search bar behaviour
+                let searchInput = sidebarNode.querySelector(".search");
+                searchInput.addEventListener("focusin", searchBarFocused);
+                searchInput.addEventListener("focusout", searchBarUnFocused);
+
+                // Inject projects and todos
+                for (let p = 0; p < projects.length; p++) {
+                    let project = projects[p];
+            
+                    let itemContainer = document.createElement("div");
+                    itemContainer.classList.add("itemContainer");
+            
+                    // Generate project html
+                    let projectTemplate = document.createElement("template");
+                    projectTemplate.innerHTML = projectSidebarTemplate(project, p);
+                    let projectNode = projectTemplate.content.querySelector("div");
+            
+                    // Make sure correct project is selected
+                    if (project.uuid === selected) {
+                        projectNode.classList.add("selected");
+                    }
+            
+                    // Add folder button click event handler
+                    let folderBtn = projectNode.querySelector(".folder.icon");
+                    folderBtn.addEventListener("click", folderBtnClicked);
+            
+                    // Add remove button click event handler
+                    let removeProjectBtn = projectNode.querySelector(".removeProjectIcon");
+                    removeProjectBtn.addEventListener("click", e => {
+                        projectRemoved(e, projects);
+                    });
+                    itemContainer.appendChild(projectNode);
+            
+                    // Generate todo html
+                    for (let t = 0; t < project.todos.length; t++) {
+                        let todoHtml = todoSidebarTemplate(project.todos[t], t);
+                        let todoTemplate = document.createElement("template");
+                        todoTemplate.innerHTML = todoHtml;
+                        let todoNode = todoTemplate.content.querySelector("div");
+                        let removeTodoBtn = todoNode.querySelector(".removeProjectIcon");
+                        removeTodoBtn.addEventListener("click", e => {
+                            todoRemoved(e, project);
+                        });
+                        itemContainer.appendChild(todoNode);
+                    } 
+                    sidebarNode.appendChild(itemContainer);
+                }
+
+                return sidebarNode;
+            },
+            render: (root) => {
+                root.replaceWith(sidebarObj.html());
+                root = document.getElementsByClassName(root.className)[0];
+
+                // Auto open the selected project if such a project exists
+                let firstProjectFolderIcon = root.querySelector(".itemContainer > .item.project.selected > .folder.icon");
+                firstProjectFolderIcon.click();
+            }
+        };
+
+        return sidebarObj;
+    })();
 };
--- src/js/index.js
+++ src/js/index.js
@@ -2,29 +2,11 @@ import "./../css/sidebar.css";
 import "./../css/todos.css";
 import "./../css/index.css";
 
-import { Project, Todo } from "./Todos";
-import { renderSidebar, createTestProjects } from "./Sidebar";
-
-function searchBarFocused(e) {
-    e.preventDefault();
-
-    let searchBar = e.target.parentElement;
-    searchBar.classList.add("focused");
-}
-
-function searchBarUnFocused(e) {
-    e.preventDefault();
-
-    let searchBar = e.target.parentElement;
-    searchBar.classList.remove("focused");
-}
-
-// Implement search bar event listeners
-const searchInput = document.getElementsByClassName("search")[0];
-searchInput.addEventListener("focusin", searchBarFocused);
-searchInput.addEventListener("focusout", searchBarUnFocused);
+import { sidebar, createTestProjects } from "./Sidebar";
 
 // Create test projects
-let projects = createTestProjects(5, 5);
-console.log(projects);
-renderSidebar(projects);
+let {projects, selected} = createTestProjects(5, 5);
+
+let root = document.getElementsByClassName("sidebar")[0];
+let sidebarNode = sidebar(projects);
+sidebarNode.render(root);
