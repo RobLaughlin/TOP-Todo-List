6a661fc19c0a9f0de59e5d064184307c26012aa3
Rob Laughlin <waterfallkicks@gmail.com>
2024-11-05 10:29:08 -0800

    Rebuild change listener data structure

--- src/js/Todos.js
+++ src/js/Todos.js
@@ -1,60 +1,48 @@
 import { v4 as uuidv4 } from 'uuid';
 
-class Listenable {
-    _validEvents = new Set();
-    _eventHandlers = new Map();
+class ChangeListener {
+    _toChange = new Map();
 
     /**
-     * Creates a set of what events are considered valid
-     * @param {Set<string>} validEvents A set of valid events
+     * Adds a callback function to be called when data is changed
+     * @param {Function(Todo)} callback The callback function to be called when data is changed
+     * @returns {string} The ID of the callback function
      */
-    constructor(validEvents) {
-        this._validEvents = validEvents;
+    addChangeListener(callback) {
+        const callbackId = uuidv4();
+        this._toChange.set(callbackId, callback);
+        return callbackId;
+    }
 
-        this._validEvents.forEach(e => {
-            this._eventHandlers.set(e, new Map());
+    /**
+     * Removes a callback function via the associated callback function ID if it exists
+     * @param {string} callbackId The ID of the callback function 
+     * @returns {?string} The ID of the callback function if such an ID exists. Returns null if the callback ID is not found
+     */
+    removeChangeListener(callbackId) {
+        if (!(this._toChange.has(callbackId))) {
+            return null;
+        }
+
+        this._toChange.delete(callbackId);
+        return callbackId;
+    }
+
+    changed() {
+        this._toChange.values().forEach(callback => {
+            callback(this);
         });
-    }
-
-    /**
-     * Adds an event listener to this object. The event id must be a valid event in Todo.EVENTS,
-     * whereas the callback id can be anything.
-     * @param {string} event The event id to trigger
-     * @param {string} id The id of the callback function
-     * @param {Function(Todo)} callback The function to be called when the event triggers
-     */
-    addEventListener(event, id, callback) {
-        if (!(event in this._validEvents)) {
-            throw new Error(`Invalid event ${event}`);
-        }
-        this._eventHandlers.get(event).set(id, callback);
-    }
-
-    /**
-     * Removes an event listener from this object. The event id must be a valid event in Todo.EVENTS,
-     * whereas the callback id can be anything.
-     * @param {string} event The event id to trigger
-     * @param {string} id The id of the callback function
-     */
-    removeEventListener(event, id) {
-        if ((this._validEvents.has(event)) && (this._eventHandlers.get(event).has(id))) {
-            this._eventHandlers.get(event).delete(id);
-        }
     }
 };
 
-export class Todo extends Listenable {
-    static EVENTS = {
-        onChange: "onChange"
-    };
-
+export class Todo extends ChangeListener {
     #title;
     #description;
     #dueDate;
     #priority;
     #notes;
     #uuid = uuidv4();
-
+    
     /**
      * Creates a Todo
      * 
@@ -65,7 +53,7 @@ export class Todo extends Listenable {
      * @param {string} [notes=""] Optional notes to include with the Todo
      */
     constructor(title, description, dueDate, priority, notes="") {
-        super(Object.values(Todo.EVENTS));
+        super();
         this.#title = title;
         this.#description = description;
         this.#dueDate = dueDate;
@@ -76,51 +64,37 @@ export class Todo extends Listenable {
     get title() { return this.#title; }
     set title(value) {
         this.#title = value;
-        this.#onChange();
+        this.changed();
     }
 
     get description() { return this.#description; }
     set description(value) {
         this.#description = value;
-        this.#onChange();
+        this.changed();
     }
 
     get dueDate() { return this.#dueDate; }
     set dueDate(value) {
         this.#dueDate = value;
-        this.#onChange();
+        this.changed();
     }
 
     get priority() { return this.#priority; }
     set priority(value) {
         this.#priority = value;
-        this.#onChange();
+        this.changed();
     }
 
     get notes() { return this.#notes; }
     set notes(value) {
         this.#notes = value;
-        this.#onChange();
+        this.changed();
     }
-
+    
     get uuid() { return this.#uuid; }
-
-    /**
-     * Calls every callback subscribed to the onChange event
-     * @private
-     */
-    #onChange() {
-        for (const [_, callback] of this._eventHandlers.get(Todo.EVENTS.get('onChange'))) {
-            callback(this);
-        }
-    }
 }
 
-export class Project extends Listenable {
-    static EVENTS = {
-        onChange: "onChange"
-    };
-
+export class Project extends ChangeListener {
     #name = ""
     #todos = []
     #uuid = uuidv4()
@@ -129,10 +103,9 @@ export class Project extends Listenable {
      * A container for an array of Todos, with a given name
      * @param {string} name The name of the project
      * @param {Todo[]} [todos=[]] The array of todos
-     * @param {?string} [onChange=null] The event to be broadcast when a todo is changed
      */
     constructor(name, todos=[]) {
-        super(Object.values(Project.EVENTS));
+        super();
         this.#name = name;
         this.#todos = [...todos];
     }
@@ -157,7 +130,7 @@ export class Project extends Listenable {
      */
     add(todo) {
         this.#todos.push(todo);
-        this.#onChange();
+        this.changed();
     }
 
     /**
@@ -169,7 +142,7 @@ export class Project extends Listenable {
             throw new RangeError("Index of todo out of range.");
         }
         this.#todos.splice(idx, 1);
-        this.#onChange();
+        this.changed();
     }
 
     /**
@@ -181,15 +154,5 @@ export class Project extends Listenable {
             throw new RangeError("Index of todo out of range.");
         }
         return this.#todos[idx];
-    }
-
-    /**
-     * Calls every callback subscribed to the onChange event
-     * @private
-     */
-    #onChange() {
-        for (const [_, callback] of this._eventHandlers.get(Project.EVENTS['onChange'])) {
-            callback(this);
-        }
     }
 } 
--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,12 @@ let {projects, selected} = createTestProjects(5, 5);
 
 let sidebarNode = createSidebar(projects);
 sidebarNode.render();
+
+projects[0].addChangeListener(project => {
+    console.log(project.todos);
+    // sidebarNode = createSidebar(projects);
+    sidebarNode.render();
+});
+projects[0].remove(0);
+projects[0].remove(0);
+// console.log(projects[0]);
