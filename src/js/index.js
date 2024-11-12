5048ad88f40597bf264ca1429342ccdf7544c0b4
Rob Laughlin <waterfallkicks@gmail.com>
2024-11-12 07:23:21 -0800

    Cleanup index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,16 +5,11 @@ import "./../css/index.css";
 import { createSidebar } from "./Sidebar/Sidebar.component";
 import { createTestProjects } from "./Sidebar/Sidebar.test";
 
+const NUM_TEST_PROJECTS = 5;
+const NUM_TODOS_PER_TEST_PROJECT = 4;
+
 // Create test projects
-let {projects, selected} = createTestProjects(5, 5);
+let {projects, selected} = createTestProjects(NUM_TEST_PROJECTS, NUM_TODOS_PER_TEST_PROJECT);
 
 let sidebarNode = createSidebar(projects);
 sidebarNode.render();
-
-projects[0].addChangeListener(project => {
-    // sidebarNode = createSidebar(projects);
-    sidebarNode.render();
-});
-projects[0].remove(0);
-projects[0].remove(0);
-// console.log(projects[0]);
